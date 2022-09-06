1d77409af1c08b60534c3ebe404b7f7029b21178
josebasgil <josebasgil29@gmail.com>
2022-09-06 12:41:35 -0500

    Commit1 for test to share to team

--- src/test/java/steps/pageInitializers.java
+++ src/test/java/steps/pageInitializers.java
@@ -1,12 +1,16 @@
 package steps;
 
+import pages.Story08JoseBasPage;
 import pages.loginPage;
 
 public class pageInitializers {
     public static loginPage login;
 
+    public static Story08JoseBasPage addEmployeeDependent;
+
     public static void initializePageObjects(){
 
         login = new loginPage();
+        addEmployeeDependent = new Story08JoseBasPage();
     }
 }
